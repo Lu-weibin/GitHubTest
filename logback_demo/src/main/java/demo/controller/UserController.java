36bf5f70b451d034a9419fbe505a1ab25ccb52ea
luwb <1571701516@qq.com>
2019-12-09 14:58:06 +0800

    新增logback_demo模块

---
+++ logback_demo/src/main/java/demo/controller/UserController.java
@@ -0,0 +1,43 @@
+package demo.controller;
+
+import demo.pojo.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by luwb on 2019/12/04.
+ * 控制器
+ */
+@Controller
+public class UserController {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@RequestMapping(value = "/login",method = RequestMethod.GET)
+	@ResponseBody
+	public Map<String,Object> login(HttpServletRequest request){
+		//日志级别从低到高分为TRACE < DEBUG < INFO < WARN < ERROR < FATAL，如果设置为WARN，则低于WARN的信息都不会输出。
+		logger.trace("日志输出 trace");
+		logger.debug("日志输出 debug");
+		logger.info("日志输出 info");
+		logger.warn("日志输出 warn");
+		logger.error("日志输出 error");
+		Map<String,Object> map =new HashMap<>();
+		String userName=request.getParameter("username");
+		String password=request.getParameter("password");
+		if(!"".equals(userName) && !"".equals(password)){
+			User user =new User(userName,password);
+			request.getSession().setAttribute("user",user);
+			map.put("result","1");
+		}else{
+			map.put("result","0");
+		}
+		return map;
+	}
+}
