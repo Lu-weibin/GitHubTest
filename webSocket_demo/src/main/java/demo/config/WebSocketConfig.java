8ea1674ff4b557c759853931693cae1b7ef3c2fd
luwb <1571701516@qq.com>
2019-12-09 12:02:22 +0800

    新增webSocket_demo模块

---
+++ webSocket_demo/src/main/java/demo/config/WebSocketConfig.java
@@ -0,0 +1,17 @@
+package demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * Created by luwb on 2019/12/04.
+ * 开启WebSocket支持
+ */
+@Configuration
+public class WebSocketConfig {
+	@Bean
+	public ServerEndpointExporter serverEndpointExporter() {
+		return new ServerEndpointExporter();
+	}
+}
