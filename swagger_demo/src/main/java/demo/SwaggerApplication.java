da3508ef383bb294c8b2fbbad1ce387fcd2b4fba
luwb <1571701516@qq.com>
2019-12-09 14:43:08 +0800

    新增swagger_demo模块

---
+++ swagger_demo/src/main/java/demo/SwaggerApplication.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by luwb on 2019/12/04.
+ * 启动类
+ */
+@SpringBootApplication
+public class SwaggerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerApplication.class, args);
+	}
+}
