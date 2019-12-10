de424e65b9769584cb74da5f07f83f6061408070
luwb <1571701516@qq.com>
2019-12-10 10:24:30 +0800

    新增shiro_JWT_demo模块

---
+++ shiro_JWT_demo/src/main/java/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package demo;
+
+import demo.properties.SystemProperties;
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+/**
+ * @author MrBird
+ */
+@SpringBootApplication
+@EnableConfigurationProperties(SystemProperties.class)
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(DemoApplication.class)
+                .bannerMode(Banner.Mode.OFF)
+                .run(args);
+    }
+
+}
