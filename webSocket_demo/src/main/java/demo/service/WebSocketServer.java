84099954f84b68d478ea3d9a014f22110ea0149c
luwb <1571701516@qq.com>
2020-01-17 14:24:15 +0800

    ”“

--- webSocket_demo/src/main/java/demo/service/WebSocketServer.java
+++ webSocket_demo/src/main/java/demo/service/WebSocketServer.java
@@ -17,23 +17,26 @@ import cn.hutool.log.LogFactory;
 @Component
 public class WebSocketServer {
 
-	static Log log=LogFactory.get(WebSocketServer.class);
+	private static Log log = LogFactory.get(WebSocketServer.class);
 	// 静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。
 	private static int onlineCount = 0;
 	//concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。
 	private static CopyOnWriteArraySet<WebSocketServer> webSocketSet = new CopyOnWriteArraySet<>();
 	// 与某个客户端的连接会话，需要通过它来给客户端发送数据
 	private Session session;
-
 	//接收sid
 	private String sid="";
+
 	/**
-	 * 连接建立成功调用的方法*/
+	 * 连接建立成功调用的方法
+	 */
 	@OnOpen
 	public void onOpen(Session session,@PathParam("sid") String sid) {
 		this.session = session;
-		webSocketSet.add(this);     //加入set中
-		addOnlineCount();           //在线数加1
+		//加入set中
+		webSocketSet.add(this);
+		//在线数加1
+		addOnlineCount();
 		log.info("有新窗口开始监听:"+sid+",当前在线人数为" + getOnlineCount());
 		this.sid=sid;
 		try {
@@ -56,9 +59,10 @@ public class WebSocketServer {
 	/**
 	 * 收到客户端消息后调用的方法
 	 *
-	 * @param message 客户端发送过来的消息*/
+	 * @param message 客户端发送过来的消息
+	 */
 	@OnMessage
-	public void onMessage(String message, Session session) {
+	public void onMessage(String message) {
 		log.info("收到来自窗口"+sid+"的信息:"+message);
 		//群发消息
 		for (WebSocketServer item : webSocketSet) {
@@ -102,7 +106,7 @@ public class WebSocketServer {
 					item.sendMessage(message);
 				}
 			} catch (IOException e) {
-				continue;
+				log.error("websocket发生异常,{}", e.getMessage());
 			}
 		}
 	}
